d43cb57e0c06c1762d9467732521c7bf493c055e
sanghyun <hsh7097@naver.com>
2021-03-12 17:02:39 +0900

    splashUiUpdate

--- app/src/main/java/com/nolbal/prehomework/MainActivity.kt
+++ app/src/main/java/com/nolbal/prehomework/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.nolbal.prehomework
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        fun start(context: Context?) {
+            context?.startActivity(
+                context.intentFor<MainActivity>()
+            )
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
